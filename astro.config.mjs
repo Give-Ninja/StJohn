bc86d4b457ecd91f5f5b6c4b2bbb88192eb9bb7b
Simon Stekler <simon.stekler@gmail.com>
2024-09-05 13:19:55 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,35 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+    integrations: [tailwind(), icon({
+      svgoOptions: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                // or disable plugins
+                removeDoctype: false,
+                margeStyles: false,
+                cleanupAttrs: false,
+                convertPathData: false,
+                convertShapeToPath: false,
+                convertStyleToAttrs: false,
+                convertTransform: false,
+                inlineStyles: false,
+                minifyStyles: false,
+                removeAttrs: false,
+                removeDimensions: false,
+                removeStyleElement: false
+              }
+            }
+          }
+        ]
+      }
+    })]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				red: '#E5173F',
+				navy: '#00092E',
+				'grey-20': '#F5F4F4',
+				'grey-60': '#5A5656',
+				'grey-80': '#312C2C',
+			},
+			fontFamily:{
+				sans: ['"Poppins"', ...defaultTheme.fontFamily.sans]
+			},
+			fontSize:{
+				'1.5xl': '1.25rem',
+				'2.5xl': '2rem'
+			},
+		},
 	},
 	plugins: [],
 };
